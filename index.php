fa4ca73506cfc00d666f244088b81ae8e9e1c095
Steven Galvez <stikgalvez@hotmail.com>
2019-05-25 12:19:56 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<?php
+$profile = [
+	"name" => "Teresa Santos",
+	"age" => "19",
+	"position" => "Junior Developer",
+	"gender" => "F",
+	"facebook_link" => "https://www.facebook.com/teresa",
+	"github_link" => "https://github.com/teresa",
+	"bio" => "Teresa is an young entrepreneur living in Miami who likes ice cream and watches superheroes movies at night. She is a happy fellow.",
+	"picture" => "https://www.imagen.com.mx/assets/img/imagen_share.png"
+];
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link  rel="stylesheet" href="style.css" >
+    <title>Document</title>
+</head>
+<body>
+    <!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+<div class="card">
+  <img src="angie1.jpeg" alt="John" style="width:100%">
+  <h1>Angie De Alba</h1>
+  <p class="title">Hermosa</p>
+  <p class="title">Es la mujer mas hermosa que mis ojos han podido ver, tan dulce como la miel,tan brillante como las estrellas,
+  Tu aire me permite respirar,
+
+tu mirada me guía por el camino correcto,
+
+tus pasos son el proyecto que soñé seguir,
+
+pero lo más importante,
+
+es poder hacerlo junto a ti.
+
+</p>
+  <p>TE AMO MI AMOR</p>
+
+  
+</div>
+</body>
+</html>
+
